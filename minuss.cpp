86d18cd4c65b62b16281ad2025434c7306078885
Furqan Qadeer <furqanqadeer30@gmail.com>
2024-09-24 12:20:36 +0500

    adding minuss file

---
+++ minuss.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, sum;
+
+    // Input two numbers
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Calculate the divide
+    minuss = num1 - num2;
+
+    // Output the result
+    cout << "The divide of " << num1 << " and " << num2 << " is " << sum << endl;
+
+    return 0;
+}
